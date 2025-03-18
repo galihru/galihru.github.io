932d482e2dc9e88ec8f306ae6367b46022a24cdf
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-03-18 09:53:09 +0700

    Create xss_scanner.go

---
+++ go/xss_scanner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+)
+
+func main() {
+	// Baca file HTML
+	content, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Regex untuk mendeteksi potensi XSS
+	xssPattern := regexp.MustCompile(`<script>[^<]+</script>`)
+	matches := xssPattern.FindAllString(string(content), -1)
+
+	if len(matches) > 0 {
+		fmt.Println("Potensi XSS ditemukan!")
+		for _, match := range matches {
+			fmt.Println(match)
+		}
+	} else {
+		fmt.Println("Tidak ditemukan potensi XSS.")
+	}
+}
