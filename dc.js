b2ea42dc5295763bd4cdec19bfcc81ef62332755
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-03-18 12:10:17 +0700

    Create dc.js

---
+++ dc.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth < 720) {
+        document.head.innerHTML += '<meta http-equiv="refresh" content="0;url=https://4211421036.github.io/mobile">';
+    }
+});
